57c09376307bdf9b2a061e20190a0c5a82201ad4
Robert Taveras <monkeyeden@Roberts-MBP.lan>
2021-06-07 21:02:45 -0400

    I did it

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,12 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+
+//Memory
+export const MEMORY_SAVED = 'MEMORY_SAVED';
+export const USE_MEMORY = 'USE_MEMORY';
+export const CLEAR_MEMORY = 'CLEAR_MEMORY';
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -10,3 +16,23 @@ export const addOne = () => {
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
 }
+
+export const changeOperator = (operator) =>{
+    return({type:CHANGE_OPERATION,payload:operator})
+}
+
+export const clearDisplay= () =>{
+    return({type:CLEAR_DISPLAY})
+} 
+
+export const saveMemory = () =>{
+    return({type:MEMORY_SAVED})
+}
+
+export const memoryUse = () =>{
+    return({type:USE_MEMORY})
+}
+export const clearMemory = () =>{
+    return({type:CLEAR_MEMORY})
+} 
+
